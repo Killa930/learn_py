372276bdab4c119f15b78bea08fa271f9ca416d4
Killa <tumanovs010@gmail.com>
2026-08-13 14:43:38 +0300

    variable

---
+++ test.py
@@ -0,0 +1,36 @@
+# print("Hello, World!")
+
+# input()
+
+# number = 5
+# print(number)
+
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
+
+# num1 += 5
+
+# print("Result:", num1 + num2)
+# print("Result:", num1 - num2)
+# print("Result:", num1 / num2)
+# print("Result:", num1 ** num2)
+# print("Result:", num1 // num2)
+
+# num1 = int(input("enter first number: "))
+# num2 = int(input("enter second number: "))
+
+# num1 *= 5
+
+# print("Result:", num1 + num2)
+# print("Result:", num1 - num2)
+# print("Result:", num1 / num2)
+# print("Result:", num1 ** num2)
+# print("Result:", num1 // num2)
+
+# word = "Hi" #String
+
+# print(word * 5)
+
+# word = True #Boolean
+
+
