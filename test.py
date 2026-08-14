8ba07c93cac7806a0d3a2945724b2a8f15b4045c
Killa <tumanovs010@gmail.com>
2026-08-14 13:25:27 +0300

    Ternory operator

--- test.py
+++ test.py
@@ -74,7 +74,16 @@
 
 # TERNARY OPERATOR
 
+# data = input()
 
+# number = 5 if data == "Five" else 0 # В одну строку пишем
+
+# # if data == "Five":
+# #     number = 5
+# # else:
+# #     number = 0
+
+# print(number)
 
 
 
