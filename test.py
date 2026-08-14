53d1c95cba4411599529e43c6f60c9cfcd0c39c9
Killa <tumanovs010@gmail.com>
2026-08-14 13:47:30 +0300

    LOOPS(WHILE,FOR)

--- test.py
+++ test.py
@@ -86,6 +86,68 @@
 # print(number)
 
 
+# LOOPS(FOR, WHILE)
+
+# for i in range(1, 6, 2):
+#     print(i)
+
+
+# count = 0
+# word = "Hello Bitch"
+# for i in word:
+#     if i == "h":
+#         count += 1
+
+# print(count)
+
+# for i in range(11):
+#     if i == 5:
+#         break
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+# found = None
+
+# for i in "hello":
+#     if i == "l":
+#         found = True
+#         break
+# else:
+#     found = False
+
+# print(found)
+
+
+
+# i = 5
+# while i < 15:
+#     print(i)
+#     i += 2
+
+
+
+# i = 5
+
+# isHasCar = True
+
+# while isHasCar:
+#     print(i)
+#     i += 1
+
+
+# isHasCar = True
+
+# while isHasCar:
+#     if input("Enter data:") == "Exit":
+#         isHasCar = False
+
+
+
+
+
+
+
 
 
 
