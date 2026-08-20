a5d17989f7b23aced6d86797798ce3b7b71d3bb8
Killa <tumanovs010@gmail.com>
2026-08-20 14:28:57 +0300

    Simple guess number game 1.1 (Modified with number input verification)

--- simple_python_game1.py
+++ simple_python_game1.py
@@ -4,15 +4,36 @@ from random import randint
 secret_number = random.randint(1, 100)
 print("Я загадал число от 1 до 100, попробуй отгадать :}")
 
-user_number = 0
 try_count = 0
 
-while user_number != secret_number:
-    try_count += 1
-    user_number = int(input(f"{try_count}-я попытка:"))
-    if user_number < secret_number:
-        print("Маловато")
-    elif user_number > secret_number:
-        print("борщ")
+while True:
 
-print("правильно это:", secret_number)
+    user_number = input("введите число: ")
+
+    if user_number == '':
+        print("Вы ничего не ввели")
+        continue
+
+    is_number = True
+    for i in user_number:
+        if i not in "0123456789":
+            is_number = False
+            break
+
+
+    if is_number == False:
+        print("Нужно целое число!")
+        continue
+
+    guess = int(user_number)
+    try_count += 1
+
+
+
+    if guess < secret_number:
+        print("Маловато")
+    elif guess > secret_number:
+        print("борщ")
+    else:
+        print(f"Поздравляем вы отгадали число {secret_number} за {try_count} попыток")
+
